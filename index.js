a212b8180cd0e2123aa0251fb9f2fc8d099cc357
alcan2jc <alcan2jc@github.com>
2022-01-24 18:04:53 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const commando = require('discord.js-commando');
+
+//const client = new Discord.Client();
+const client = new commando.Client();
+
+client.commandPrefix = '$';
+client.unknownCommandResponse = false;
+client.registry.registerGroup("games", "Games");
+client.registry.registerGroup("music", "Music");
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + "/commands");
+
+
+client.on('ready', () => {
+	console.log('Bot is now connected');
+	client.user.setActivity("bruh");
+	client.channels.cache.find(x => x.name === 'test').send('ready');
+});
+
+client.on('message', (msg) => {
+
+	//Ignores all bots
+	if (msg.author.bot) {
+		return;
+	}
+});
+client.login(process.env.token);
